3e9e1edb491a2fae8b67486fceb056c2648a1bef
Philip N <phi.nguyen@laverne.edu>
2017-05-24 22:12:56 -0700

    Add demo for get input value on btn click

--- app.js
+++ app.js
@@ -18,16 +18,40 @@ var shoppingListController = (function() {
 
 
 var UIController = (function() {
-console.log ('started auto func');
+  return {
+    getInput: function() {
+            var value = document.querySelector('.add__description').value;
+            return value;
+    }
+  }
+
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function(shoppingListCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log ('you clikeddd!');
-  });
+  var ctrlAddItem = function() {
+    // after btn is clicked:
+    // 1. get input field input data
+    UICtrl.getInput();
+    console.log ('just called UICtrl.getInput, value is:',     UICtrl.getInput());
+
+    // 2. Add data to to cntroller
+
+    // 3. Add data to UI
+    console.log ('It works. ctrlAddItem being run...');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  // console.log ('you clicked add__btn. Here is the value:', UICtrl.getInput());
+  console.log ('test value', UICtrl.getInput());
+
+  // document.querySelectorAll('.test_click').addEventListener('click', function() {
+  //   console.log ('you clicked test_click. Here is the value:');
+  // });
+
 
   // document.querySelector('.add__custom__btn').addEventListener('click', function() {
   //   console.log ('you clikeddd! custum btn');
@@ -36,6 +60,7 @@ var controller = (function(shoppingListCtrl, UICtrl) {
   document.addEventListener('keypress', function(event){
     if (event.keyCode === 13 || event.which === 13) {
       console.log ('enter key was pressed');
+      ctrlAddItem();
     }
   });
 
