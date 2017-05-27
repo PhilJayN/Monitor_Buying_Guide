5a052f81f69344c26b86420b8b5ec7fa4bf0377f
Philip N <phi.nguyen@laverne.edu>
2017-05-26 22:08:57 -0700

    Implement extraction of of btn txt when clicking on assorted btn txt btn

--- app.js
+++ app.js
@@ -59,12 +59,13 @@ var controller = (function(shoppingListCtrl, UICtrl) {
     // after btn is clicked:
     // 1. get input field input data
     UICtrl.getInput();
-    console.log ('just called UICtrl.getInput:',     UICtrl.getInput().customValue);
+    // console.log ('just called UICtrl.getInput:',     UICtrl.getInput().customValue);
 
     // 2. Add data to to cntroller
 
     // 3. Add data to UI
-    console.log ('It works. ctrlAddItem being run...');
+    // console.log ('It works. ctrlAddItem being run...');
+    console.log ('btn text content asdf:', UICtrl.getInput().btnText );
 
     // add event listener to parent. if the specific target element is of a certain type, or id, or name,
     // then run a code
@@ -74,7 +75,7 @@ var controller = (function(shoppingListCtrl, UICtrl) {
 
 
   // console.log ('you clicked add__btn. Here is the value:', UICtrl.getInput());
-  console.log ('test value', UICtrl.getInput());
+  // console.log ('test value', UICtrl.getInput());
 
   // document.querySelectorAll('.test_click').addEventListener('click', function() {
   //   console.log ('you clicked test_click. Here is the value:');
@@ -111,11 +112,15 @@ jumbo.addEventListener('click', function(event) {
     console.log ('you found the add btn!');
   }
 
+
+
   // if (event.target.className === 'teddy') {
   //   console.log ('you found the add btn!');
   // }
   // var target = event.target;
 });
+
+
 
 
 //
